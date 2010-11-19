e6820e136ff336b72749310ee5df3771f58983fa
Torkild Retvedt <torkild@retvedt.no>
2010-11-19 12:04:12 +0100

    Force can overwrite existing files/symlinks

--- make_links.sh
+++ make_links.sh
@@ -4,13 +4,26 @@ userdir=$(echo ~)
 curdir=$(pwd)
 reldir=${curdir:${#userdir}+1}
 
+force=0
+
+while getopts ":f" opt; do
+    case $opt in
+        f)
+            force=1
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
 for file in $(find -type f ! -path "./.git/*" -a -! -path $0); do
 
     base=${file:2}
 
-    if [ ! -f ~/${file} ]; then
+    if [ $force -eq 1 -o ! -f ~/${file} ]; then
         echo "Creating symlink: ${base} -> ~/${base}"
-        ln -s "$curdir/${base}" ~/${base}
+        ln -sf "$curdir/${base}" ~/${base}
     else
         echo "Symlink already in place: ~/${base}"
     fi
